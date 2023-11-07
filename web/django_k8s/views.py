562b50a1065ed683ec05645c4617d3095260d2e3
ahmadmsvh <ahmadmsvh@gmail.com>
2023-11-07 11:04:50 +0330

    portfolio

--- web/django_k8s/views.py
+++ web/django_k8s/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def portfolio(request):
-    response = '''<img src="../k8s/nginx-test/statics/statics/d20230107_194548.jpg" alt="" style="height: 100px; float: left;">
+    response = '''<img src="../../k8s/nginx-test/statics/statics/d20230107_194548.jpg" alt="" style="height: 100px; float: left;">
 <div style="height: 100px; background-color: rgb(92, 132, 192); display: flex; align-items: center;
 justify-content: center;">
     <h1 style="color: aliceblue;">This is going to be a fabulous blog for programmrs</h1>
