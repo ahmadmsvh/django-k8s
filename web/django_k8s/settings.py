b3f25e49f4e09c057e6f9316e4a9f108f749fb92
ahmadmsvh <ahmadmsvh@gmail.com>
2023-11-10 13:03:50 +0330

    collectstatic

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     
 ]
 
@@ -173,3 +174,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+
+STATICFILES_DIRS= [
+    BASE_DIR / "staticfiles"
+
+]
+from .cdn.conf import *
