0f00d6aea8532a27d80b8ec4d1652a3e3d14d6e3
ahmadmsvh <ahmadmsvh@gmail.com>
2023-11-11 14:28:38 +0330

    width correction

--- web/django_k8s/views.py
+++ web/django_k8s/views.py
@@ -10,7 +10,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def portfolio(request):
-    response = '''<img src='https://django-k8s.ams3.digitaloceanspaces.com/static/Ahmad-MousaviHaghdoust.jpg' alt="" style="height: 100px; float: left;">
+    response = '''<div style="display: flex; align-items: center;
+justify-content: center;"><div style="width:70%;"><img src='https://django-k8s.ams3.digitaloceanspaces.com/static/Ahmad-MousaviHaghdoust.jpg' alt="" style="height: 100px; float: left;">
 <div style="height: 100px; background-color: rgb(92, 132, 192); display: flex; align-items: center;
 justify-content: center;">
     <h1 style="color: aliceblue;">This is going to become a fabulous blog for programmrs</h1>
@@ -18,7 +19,7 @@ justify-content: center;">
 <div style="height: 100px; background-color: rgb(57, 77, 107); display: flex; align-items: center;
 justify-content: center;">
     <h1 style="color: aliceblue;">django/devops</h1>
-</div>
+</div></div><div/>
 '''
     # cats_load.run() 
     return HttpResponse(response)
